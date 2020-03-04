7ec52fa5cc2437f8df9df678892dc5a82f98601f
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-04 17:27:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,6 +64,16 @@ class Dog
     DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
  end 
  
+ def self.find_by_id(id)
+   sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+ 
+    DB[:conn].execute(sql, id).map { |row| self.new_from_db(row) }.first
+ end 
+ 
  def save
    
  end 
