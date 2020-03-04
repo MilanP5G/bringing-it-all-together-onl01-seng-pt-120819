549ce8478ca2c9b69c05d91018d0c9df4c18c98b
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-04 17:03:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,22 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   
-def initialize(id:nil, name:, breed:)
-  @id = id
-  @name = name 
-  @breed = breed
-end 
-  
+ def initialize(id:nil, name:, breed:)
+   @id = id
+   @name = name 
+   @breed = breed
+ end 
+ 
+ def self.create_table
+   sql = <<-SQL 
+    CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT)
+    SQL
+    
+    DB
+ end 
   
   
   
