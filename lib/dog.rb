d28e4e74ffc6801e9b19d14eef409e12ccff06db
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-04 17:35:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ class Dog
       id = dog_data[0]
       name = dog_data[1]
       breed = dog_data[2]
-      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = Dog.new(id:id, name:name, breed:breed)
     else
       dog = self.create(name: name, breed: breed)
     end
